2619f104c946a0d145555273ff7b11925d795dc0
Dilshan <dsgoo1@student.monash.edu>
2021-10-22 14:58:35 +1100

    uploading files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Final Year Project
 Final year project participating in BraTS 2021 Challenge
 
-This repo contains the code used to train a two-stage architecture as described in the final report
+This repo contains the code used to train a two-stage architecture
+
+![architecture diagram](./architecture.png)
---
+++ loss.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+import torch
+import numpy as np
+
+class DiceLoss(nn.Module):
+    def __init__(self, eps: float = 1e-9):
+        super(DiceLoss, self).__init__()
+        self.eps = eps
+        
+    def forward(self,
+                logits: torch.Tensor,
+                targets: torch.Tensor) -> torch.Tensor:
+        num = targets.size(0)
+        pred = torch.sigmoid(logits)
+        probability = pred.view(num, -1)
+        targets = targets.view(num, -1)
+        assert(probability.shape == targets.shape)
+        
+        intersection = 2.0 * (probability * targets).sum()
+        union = probability.sum() + targets.sum()
+        dice_score = (intersection) / (union + self.eps)
+        return 1.0 - dice_score
+    
+def dice_coef_metric(probabilities: torch.Tensor,
+                     truth: torch.Tensor,
+                     treshold: float = 0.5,
+                     eps: float = 1e-9) -> np.ndarray:
+    """
+    Calculate Dice score for data batch.
+    Params:
+        probobilities: model outputs after activation function.
+        truth: truth values.
+        threshold: threshold for probabilities.
+        eps: additive to refine the estimate.
+        Returns: dice score aka f1.
+    """
+    scores = []
+    num = probabilities.shape[0]
+    predictions = (probabilities >= treshold).float()
+    assert(predictions.shape == truth.shape)
+    for i in range(num):
+        prediction = predictions[i]
+        truth_ = truth[i]
+        intersection = 2.0 * (truth_ * prediction).sum()
+        union = truth_.sum() + prediction.sum()
+        if truth_.sum() == 0 and prediction.sum() == 0:
+            scores.append(1.0)
+        else:
+            scores.append(((intersection + eps) / union).item())
+    return np.mean(scores)
